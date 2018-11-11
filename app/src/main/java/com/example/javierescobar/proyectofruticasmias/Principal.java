e534da6735c2316d236c14fc558e90194b39e6c0
javier12 <javieresos12@gmail.com>
2018-11-11 18:39:26 -0500

    Avances de Interfaz

---
+++ app/src/main/java/com/example/javierescobar/proyectofruticasmias/Principal.java
@@ -0,0 +1,59 @@
+package com.example.javierescobar.proyectofruticasmias;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Principal extends AppCompatActivity {
+    private ListView lv;
+    private String[] opc;
+    private Intent in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        lv= findViewById(R.id.IdOpciones);
+        opc= getResources().getStringArray(R.array.opcioneslistar);
+
+        ArrayAdapter<String> adapter =new ArrayAdapter(this, android.R.layout.simple_list_item_1,opc);
+
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        in = new Intent( Principal.this, Agregar_Producto.class);
+                        startActivity(in);
+                        break;
+
+                    case 1:
+                        in = new Intent(Principal.this, Catalogo_Producto.class);
+                        startActivity(in);
+                        break;
+
+                    case 2:
+                        in = new Intent( Principal.this, Reporte.class);
+                        startActivity(in);
+                        break;
+                    case 3:
+                        in = new Intent( Principal.this, Ventas.class);
+                        startActivity(in);
+                        break;
+                    case 4:
+                        System.exit(0);
+                        break;
+                }
+            }
+        });
+
+    }
+}
