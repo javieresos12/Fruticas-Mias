c739b3fc2b74c4a6eeb4e5bce89724cf5fe47307
javier12 <javieresos12@gmail.com>
2018-11-17 10:44:31 -0500

    boton limpiar de agregar producto

--- app/src/main/java/com/example/javierescobar/proyectofruticasmias/Agregar_Producto.java
+++ app/src/main/java/com/example/javierescobar/proyectofruticasmias/Agregar_Producto.java
@@ -2,12 +2,28 @@ package com.example.javierescobar.proyectofruticasmias;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Agregar_Producto extends AppCompatActivity {
+
+    EditText nombreProducto, precio, descripcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar__producto);
+        nombreProducto= findViewById(R.id.txtnombrep);
+        precio= findViewById(R.id.txtprecio);
+        descripcion= findViewById(R.id.txtDescripcion);
+    }
+
+   public void limpiar(View v ){
+        nombreProducto.setText("");
+        precio.setText("");
+        descripcion.setText("");
+        nombreProducto.requestFocus();
+
     }
 }
